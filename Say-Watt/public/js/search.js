3017f523ce7c686c25dfb3633e8eb68cdefd72db
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-11-28 21:06:15 -0800

    Make the search count results

--- Say-Watt/public/js/search.js
+++ Say-Watt/public/js/search.js
@@ -1,17 +1,25 @@
 const urlParams = new URLSearchParams(window.location.search);
 const query = urlParams.get('q');
 const lowerQuery = query.toLowerCase();
-document.getElementById('q').value = query;
-const newTitle = document.createElement('title');
-newTitle.innerHTML = query + ' &#8211; Say Watt';
-document.head.append(newTitle);
-const results = document.getElementById('results');
-fetch('/json/pages.json')
-  .then(data => data.json())
-  .then((data) => {
-    data.forEach(element => {
-      if (element.title.toLowerCase().includes(lowerQuery) || element.description.toLowerCase().includes(lowerQuery) || element.content.toLowerCase().includes(lowerQuery)) {
-        results.innerHTML += `<div class="jumbotron"><h3><a href="${element.link}">${element.title}</a></h3><p>${element.description}</p></div>`;
-      }
+if (query.length >= 1) {
+  document.getElementById('q').value = query;
+  const newTitle = document.createElement('title');
+  newTitle.innerHTML = query + ' &#8211; Say Watt';
+  document.head.append(newTitle);
+  const results = document.getElementById('results');
+  var numResults = 0;
+  fetch('/json/pages.json')
+    .then(data => data.json())
+    .then((data) => {
+      data.forEach(element => {
+        if (element.title.toLowerCase().includes(lowerQuery) || element.description.toLowerCase().includes(lowerQuery) || element.content.toLowerCase().includes(lowerQuery)) {
+          results.innerHTML += `<div class="jumbotron"><h3><a href="${element.link}">${element.title}</a></h3><p>${element.description}</p></div>`;
+          numResults++;
+        }
+        document.getElementById('title').innerHTML = `${numResults} result${numResults > 1 ? 's' : ''} for ${query}:`;
+      });
     });
-  });
+} else {
+  console.error('The search query must be at least 1 character.');
+  document.write('The search query must be at least 1 character.')
+}
