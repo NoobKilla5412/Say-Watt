e08634d826f8f8d96ff2ad105246a7ed2d8b03ee
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-11-30 19:39:49 -0800

    Add abum's bio

--- Say-Watt/public/js/about.js
+++ Say-Watt/public/js/about.js
@@ -3,6 +3,6 @@ fetch('/json/members.json')
   .then(data => data.json())
   .then((data) => {
     data.forEach(element => {
-      members.innerHTML += `<div class="jumbotron"><p>${element.name}</p><p>${element.bio}</p></div>`;
+      members.innerHTML += `<div class="jumbotron"><p>${element.name}</p><p>${element.bio}</p><p><img src="image/members/${element.name.toLowerCase()}.png" width="100px"/></p></div>`;
     });
   });
