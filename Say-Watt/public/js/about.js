7049c5dc61cc7a71801c5ad34336ef9efc68291e
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-11-30 18:47:25 -0800

    Update about.js

--- Say-Watt/public/js/about.js
+++ Say-Watt/public/js/about.js
@@ -3,6 +3,6 @@ fetch('/json/members.json')
   .then(data => data.json())
   .then((data) => {
     data.forEach(element => {
-      days.innerHTML += `<div class="jumbotron"><p>${element.name}</p><p>${element.bio}</p></div>`;
+      members.innerHTML += `<div class="jumbotron"><p>${element.name}</p><p>${element.bio}</p></div>`;
     });
   });
